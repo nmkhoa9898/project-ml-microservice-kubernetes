84cfd5278c4dccc30a3e86a8bcc30f14d09d2219
Khoa Nguyen <nguyenminhkhoa.nmk98@gmail.com>
2023-05-09 09:47:55 +0000

    upload docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="omega9898/udacity-flaskapp"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag flaskapp "${dockerpath}:flask1"
 
 # Step 3:
 # Push image to a docker repository
+docker push "${dockerpath}:flask1"
