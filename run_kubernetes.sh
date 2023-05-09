aca2e4ab092cf299831cf20e0baa50e28d32202c
Khoa - Nguyen Minh <nguyenminhkhoa.nmk98@gmail.com>
2023-05-09 17:12:11 +0700

    configure k8s to run locally, deploy k8s and save output logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="omega9898/udacity-flaskapp"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment flaskdeployed --image="${dockerpath}:flask1"
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/flaskdeployed 8000:80
